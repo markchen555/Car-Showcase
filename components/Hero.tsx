b945c75f1d01887eb2e5adc3d87c843cca6adf87
Mark Chen <markchen555@gmail.com>
2023-06-28 23:56:06 -0700

    [FE] update import paths

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import React from "react";
-import CustomButton from "./CustomButton";
+import { CustomButton } from "./";
 
 const Hero = () => {
   const handleScroll = () => {};
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-import CustomButton from "./CustomButton";
+import { CustomButton } from "./";
 
 const Navbar = () => {
   return (
