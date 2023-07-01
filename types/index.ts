add6b728ea483d0edd84a1863162df5098f0eb11
Mark Chen <markchen555@gmail.com>
2023-06-30 17:40:57 -0700

    [FE] completed basic car card component

--- types/index.ts
+++ types/index.ts
@@ -3,8 +3,11 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
 	title: string;
 	containerStyles?: string;
+	textStyles?: string;
+	rightIcon?: string;
 	handleClick?: MouseEventHandler<HTMLButtonElement>;
 	btnType?: "button" | "submit";
+	isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
