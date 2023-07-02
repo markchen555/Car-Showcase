1916fc8138c1f47f6f41ce507d879e1784fd3000
Mark Chen <markchen555@gmail.com>
2023-07-02 03:23:33 -0700

    [FE] adding search param to server side search logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
-import Image from "next/image";
+import { HomeProps } from "@/types";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 
-export default async function Home() {
-  const allCars = await fetchCars(); // the data is show on server becase it's server side rendering
+export default async function Home({ searchParams }: HomeProps) {
+  // the data is show on server becase it's server side rendering
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,18 @@
 import { CarProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+	const { manufacturer, year, model, limit, fuel } = filters;
+
+	// Set the required headers for the API request
 	const headers = {
 		'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-	const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { headers: headers });
+	// Set the required headers for the API request
+	const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, { headers: headers });
+
+	// Parse the response as JSON
 	const result = await response.json();
 
 	return result
