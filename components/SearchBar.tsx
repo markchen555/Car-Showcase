02ce257d39f465b03440039507ad10451ba81c6f
Mark Chen <markchen555@gmail.com>
2023-07-03 21:31:52 -0700

    [FE] added client side approach

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,8 +3,9 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+
 import { SearchManufacturer } from "./";
+import { SearchBarProps } from "@/types";
 
 // the resuable SearchButton component is inside SearchBar component is because they only reuse inside SearchBar component
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
@@ -19,58 +20,29 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   </button>
 );
 
-const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState("");
-  const [model, setModel] = useState("");
-
-  const router = useRouter();
+const SearchBar = ({ setManuFacturer, setModel }: SearchBarProps) => {
+  const [searchModel, setSearchModel] = useState("");
+  const [searchManufacturer, setSearchManufacturer] = useState("");
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (manufacturer.trim() === "" && model.trim() === "") {
+    if (searchManufacturer.trim() === "" && searchModel.trim() === "")
       return alert("Please provide some input");
-    }
 
-    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
-  };
-
-  const updateSearchParams = (model: string, manufacturer: string) => {
-    // Create a new URLSearchParams object using the current URL search parameters
-    const searchParams = new URLSearchParams(window.location.search);
-
-    // Update or delete the 'model' search parameter based on the 'model' value
-    if (model) {
-      searchParams.set("model", model);
-    } else {
-      searchParams.delete("model");
-    }
-
-    // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
-    if (manufacturer) {
-      searchParams.set("manufacturer", manufacturer);
-    } else {
-      searchParams.delete("manufacturer");
-    }
-
-    // Generate the new pathname with the updated search parameters
-    const newPathname = `${
-      window.location.pathname
-    }?${searchParams.toString()}`;
-
-    router.push(newPathname);
+    setModel(searchModel);
+    setManuFacturer(searchManufacturer);
   };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
         <SearchManufacturer
-          manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
+          manufacturer={searchManufacturer}
+          setManufacturer={setSearchManufacturer}
         />
         <SearchButton otherClasses="sm:hidden" />
       </div>
-
       <div className="searchbar__item">
         <Image
           src="/model-icon.png"
@@ -82,8 +54,8 @@ const SearchBar = () => {
         <input
           type="text"
           name="model"
-          value={model}
-          onChange={(e) => setModel(e.target.value)}
+          value={searchModel}
+          onChange={(e) => setSearchModel(e.target.value)}
           placeholder="Tiguan..."
           className="searchbar__input"
         />
--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,5 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtonProps {
-	title: string;
-	containerStyles?: string;
-	textStyles?: string;
-	rightIcon?: string;
-	handleClick?: MouseEventHandler<HTMLButtonElement>;
-	btnType?: "button" | "submit";
-	isDisabled?: boolean;
-}
-
-export interface SearchManufacturerProps {
-	manufacturer: string;
-	setManufacturer: (manufacturer: string) => void;
-}
-
 export interface CarProps {
 	city_mpg: number;
 	class: string;
@@ -30,6 +15,13 @@ export interface CarProps {
 	year: number;
 }
 
+export type CarState = CarProps[] & { message?: string };
+
+export interface SearchBarProps {
+	setManuFacturer: (manufacturer: string) => void;
+	setModel: (model: string) => void;
+}
+
 export interface FilterProps {
 	manufacturer?: string;
 	year?: number;
@@ -38,8 +30,24 @@ export interface FilterProps {
 	fuel?: string;
 }
 
-export interface HomeProps {
-	searchParams: FilterProps;
+export interface CarCardProps {
+	model: string;
+	make: string;
+	mpg: number;
+	transmission: string;
+	year: number;
+	drive: string;
+	cityMPG: number;
+}
+
+export interface CustomButtonProps {
+	isDisabled?: boolean;
+	btnType?: "button" | "submit";
+	containerStyles?: string;
+	textStyles?: string;
+	title: string;
+	rightIcon?: string;
+	handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface OptionProps {
@@ -47,12 +55,18 @@ export interface OptionProps {
 	value: string;
 }
 
-export interface CustomFilterProps {
-	title: string;
+export interface CustomFilterProps<T> {
 	options: OptionProps[];
+	setFilter: (selected: T) => void;
 }
 
 export interface ShowMoreProps {
 	pageNumber: number;
 	isNext: boolean;
+	setLimit: (limit: number) => void;
+}
+
+export interface SearchManufacturerProps {
+	manufacturer: string;
+	setManufacturer: (selected: string) => void;
 }
