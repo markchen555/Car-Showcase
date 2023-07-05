b2a5328ee0398584843a363c6fb9112c75911b0d
Mark Chen <markchen555@gmail.com>
2023-07-05 01:52:09 -0700

    [FE] move api key to env file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
 	images: {
 		domains: ['cdn.imagin.studio']
 	},
 	env: {
-		NEXT_PUBLIC_RAPID_API_KEY: 'TgK4nvj1RHmshQNQhRqimB2xz0p4p1Xk55cjsnyAjQ38y9arff',
-		NEXT_PUBLIC_IMAGIN_API_KEY: "hrjavascript-mastery"
+		NEXT_PUBLIC_RAPID_API_KEY: process.env.NEXT_PUBLIC_RAPID_API_KEY,
+		NEXT_PUBLIC_IMAGIN_API_KEY: process.env.NEXT_PUBLIC_IMAGIN_API_KEY,
 	  },
 }
 
